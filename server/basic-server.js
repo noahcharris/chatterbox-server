b94105721d8ed8e51c601123fc90b73262b01801
Noah Harris <noah.christopher.harris@gmail.com>
2013-12-07 10:56:12 -0800

    Changed port for heroku.

--- server/basic-server.js
+++ server/basic-server.js
@@ -5,7 +5,7 @@ var handler = require('./request-handler');
  * standard port for HTTP servers is port 80, but that port is
  * normally already claimed by another server and/or not accessible
  * so we'll use a higher port number that is not likely to be taken: */
-var port = 8080;
+var port = process.env.PORT || 8000;
 
 /* For now, since you're running this server on your local machine,
  * we'll have it listen on the IP address 127.0.0.1, which is a
